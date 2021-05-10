0fba94e672b23169cb164f0abe8664d7dbe61039
Kravitz Lab <alexxai@wustl.edu>
2021-05-10 10:15:07 -0500

    Add files via upload

--- examples/FORCE/FORCE.ino
+++ examples/FORCE/FORCE.ino
@@ -5,8 +5,7 @@
   alexxai@wustl.edu
   May, 2021
   
- KravitzLabDevices/FORCE_library is licensed under the
- GNU General Public License v3.0
+ KravitzLabDevices/FORCE_library is licensed under the GNU General Public License v3.0
 
  Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works 
  and modifications, which include larger works using a licensed work, under the same license. Copyright and license 
@@ -18,7 +17,7 @@
 //                                        DON'T EDIT THESE LINES
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <Force.h>                                      //Include FORCE library 
-String ver = "v1.1.0";                                  //unique identifier text
+String ver = "v1.1.1";                                  //unique identifier text
 Force force(ver);                                       //start FORCE object
 
 void setup() {
@@ -34,12 +33,15 @@ void loop() {
   if (force.pressLength > force.hold_time) {                  //if the force lever is held down for longer than the hold_req
     force.presses++;                                          //keep count of successful presses
     if (force.presses == force.ratio) {                       //if the ratio for number of presses is met
+
+      //This code block runs only if it's a progressive ratio session
       if (force.PR == true) {                                 //if it's a progressive ratio session
         if ((force.trial % force.trials_per_block) == 0) {    //if the number of trials_per_block is reached 
-          force.req = force.req + 2;                          //inrease force requirement 
-          if (force.req > force.max_force) force.req = 2;     // if force.req breaks max_force, reset to 2g
+          force.req = force.req + 2;                          //increase force requirement 
+          if (force.req > force.max_force) force.req = 2;     //if force.req breaks max_force, reset to 2g
         }
       }
+      
       force.Dispense();                                       //dispense reward
       force.presses = 0;                                      //reset presses
     }
