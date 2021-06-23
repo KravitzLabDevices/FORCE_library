67c557b42f620afe913f78f544a70b16044f4917
Kravitz Lab <alexxai@wustl.edu>
2021-06-22 21:56:11 -0500

    Add files via upload

--- examples/FORCE_RandomOut/FORCE_RandomOut.ino
+++ examples/FORCE_RandomOut/FORCE_RandomOut.ino
@@ -16,37 +16,38 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                        DON'T EDIT THESE LINES
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#include <Force.h>                                      //Include FORCE library 
-String ver = "v1.2.0_randOut";                          //unique identifier text
-Force force(ver);                                       //start FORCE object
+#include <Force.h>                                          //Include FORCE library 
+String ver = "v1.2.0_randOut";                              //unique identifier text
+Force force(ver);                                           //start FORCE object
 
 void setup() {
-  force.begin();                                        //setup FORCE
+  force.begin();                                            //setup FORCE
 }
 
 void loop() {
-  force.run();                                          //call force.run() at least once per loop
+  force.run();                                              //call force.run() at least once per loop
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //                                      WRITE CUSTOM BEHAVIORAL CODE BELOW HERE
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  if (force.pressLength > force.hold_time) {                  //if the force lever is held down for longer than the hold_req
+  if (force.pressLength > force.hold_time) {                //if the force lever is held down for longer than the hold_req
     //Choose a random number
-    randomSeed(millis());                                     //get a new randomSeed here so the sequence isn't the same every time
-    int randnum = random(1, 4);                               //Pick a random number between 1 and 3
-    Serial.print("Random Num: ");                             //Print the random number to the Serial Monitor
-    Serial.println(randnum);
+    randomSeed(millis());                                   //get a new randomSeed here so the sequence isn't the same every time
+    force.random_number = random(1, 4);                     //Pick a random number between 1 and 3
 
-    force.presses++;                                          //add 1 to force.presses
-    //force.Dispense();                                         //dispense reward
+    Serial.print("Random Num: ");                           //Print the random number to the Serial Monitor
+    Serial.println(force.random_number);
 
-    if (randnum == 3) { //if random number is 3...
+    force.presses++;                                        //add 1 to force.presses
+
+    if (force.random_number == 3) { //if random number is 3...
+      force.shock = true;
       force.Tone();
-      while ((force.unixtime - force.dispenseTime) < 15) {    //...and it is within 15 seconds of a dispense
+      while ((force.unixtime - force.dispenseTime) < 15) {  //...and it is within 15 seconds of a dispense
         force.run();
 
         //keep force screen and timers running while waiting around for the 15sec delay
-        if (force.lick == false) {                             //if animal licks in this time period:
+        if (force.lick == false) {                          //if animal licks in this time period:
 
           /////////////////////////////////////////////////
           // FORCE does this when both conditions are true
@@ -55,13 +56,12 @@ void loop() {
           digitalWrite (6, HIGH);
           delay(100);
           digitalWrite (6, LOW);
-
           break;
         }
       }
-      force.Dispense();
     }
-
-    force.Timeout(force.timeout_length);                      //timeout (length in seconds)
+    force.Dispense();
+    force.shock = false;
+    force.Timeout(force.timeout_length);                    //timeout (length in seconds)
   }
 }
